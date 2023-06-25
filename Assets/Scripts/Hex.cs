b29b0b8d165d07c8d1bd343989f4ba0913c0b081
Book-Shelf <arthuradd@yahoo.com>
2023-06-25 17:06:49 +0200

    Little refactor

--- Assets/Scripts/Hex.cs
+++ Assets/Scripts/Hex.cs
@@ -3,23 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hex{
+    public readonly int Q; // x
+    public readonly int R;  // y
 
-public Hex (int q, int r){
-    this.Q = q;
-    this.R = r;
-}
+    public Hex (int q, int r){
+        this.Q = q;
+        this.R = r;
+    }
 
-public readonly int Q; // x
-public readonly int R;  // y
+    public Vector2 Position(Vector2 size){
+        float radius = size.x/2;
+        //float y_1 = size.y * 0.6f;
 
+        float x = radius * (1.5f) * this.Q ;
+        float y = radius * (Mathf.Sqrt(3)/2 * this.Q  +  Mathf.Sqrt(3) * this.R);
 
-public Vector2 Position(Vector2 size){
-    float radius = size.x/2;
-    //float y_1 = size.y * 0.6f;
-
-    float x = radius * (1.5f) * this.Q ;
-    float y = radius * (Mathf.Sqrt(3)/2 * this.Q  +  Mathf.Sqrt(3) * this.R);
-
-    return new Vector2(x, y);
-}
+        return new Vector2(x, y);
+    }
 }
--- Assets/Scripts/VolcanoEruption.cs
+++ Assets/Scripts/VolcanoEruption.cs
@@ -5,40 +5,64 @@ using UnityEngine;
 
 public class VolcanoEruption : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public List<GameObject> HexBag= new List<GameObject>();
-    public List<(int dq, int dr)> HexNeighbours = new List<(int dq, int dr)>{(1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1), (0, 1)};
+    public List<GameObject> hexBag= new List<GameObject>();
+    public List<(int dq, int dr)> hexNeighbours = new List<(int dq, int dr)>{(1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1), (0, 1)};
+    Object prefabLava;
+    Transform parentObject;
+    bool isRunning = true;
 
     void Start()
     {
-        HexBag = GameObject.FindGameObjectsWithTag("Hex").ToList();
-        RemoveLavaFilled();
-
+        // HexBag = GameObject.FindGameObjectsWithTag("Hex").ToList();
+        // RemoveLavaFilled();
+        prefabLava = Resources.Load<Object>("Prefabs/Lava");
+        parentObject = GameObject.FindGameObjectsWithTag("Map")[0].transform;
+        HexMap.GenerateMap();
         //wrzuć do worka wszystkie sąsiadujące
     }
 
     // Update is called once per frame
 
-void RemoveLavaFilled()
-{
-    List<GameObject> lavaFilled = new List<GameObject>();
-        foreach (GameObject hex in HexBag)
+    public void Update()
+    {
+        if (isRunning)
         {
-            if (hex.transform.childCount >1)
+            StartCoroutine(MyCoroutine());
+        }
+    }
+
+    public IEnumerator MyCoroutine()
+    {
+        isRunning = false;
+        yield return new WaitForSeconds(5);
+        Debug.Log(GameObject.FindGameObjectsWithTag("Hex")[0].name == "Hex -6,0");
+        GameObject[] gameObject = GameObject.FindGameObjectsWithTag("Hex");
+        Debug.Log(System.Array.IndexOf(GameObject.FindGameObjectsWithTag("Hex"), System.Array.Find(gameObject, element => element.name == "Hex 0,0")));
+        int ran = Random.Range(-6, 7);
+        Debug.Log(ran);
+        isRunning = true;
+    }
+
+    void RemoveLavaFilled()
+    {
+        List<GameObject> lavaFilled = new List<GameObject>();
+            foreach (GameObject hex in hexBag)
             {
-                lavaFilled.Add(hex);
+                if (hex.transform.childCount >1)
+                {
+                    lavaFilled.Add(hex);
+                }
             }
-        }
-        foreach (GameObject doomedHex in lavaFilled)
-        {
-                HexBag.Remove(doomedHex);
-        }
-}
+            foreach (GameObject doomedHex in lavaFilled)
+            {
+                    hexBag.Remove(doomedHex);
+            }
+    }
 
     void AddToBag()
     {
         List<GameObject> lavaFilled = new List<GameObject>();
-        foreach (GameObject hex in HexBag)
+        foreach (GameObject hex in hexBag)
         {
             if (hex.transform.childCount >1)
             {
@@ -47,8 +71,8 @@ void RemoveLavaFilled()
         }
         foreach (GameObject lavaHex in lavaFilled)
         {
-                String name = lavaHex.name;
-                for (var neighbour in HexNeighbours)
+                System.String name = lavaHex.name;
+                foreach (var neighbour in hexNeighbours)
                 {
 
                 }
@@ -57,12 +81,14 @@ void RemoveLavaFilled()
         //znajdź te zalane lawą
         //przeiteruj po sąsiadach
         //jeśli nie są zalani lawą dorzuć do worka token
-    }
+        }
+        
     GameObject DrawFromBag()
     {
-//losowy element z worka
+    //losowy element z worka
         return null;
     }
+
     void LavaToChosenHex(GameObject hex)
     {
         //dołóż lawę do heksa
